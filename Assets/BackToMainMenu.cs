3e58f61e6f139761b02da2f61816ab688f1cc70d
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2017-12-15 14:47:34 -0200

    Primeira versão da página

--- Assets/BackToMainMenu.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class BackToMainMenu : MonoBehaviour {
-
-	public void Back() {
-        SceneManager.LoadScene("MainMenu");
-    }
-}
